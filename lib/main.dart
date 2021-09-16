87ef6c8284fb76355d0ce0c755911f92363d94bd
Chirag <andrag.apps@gmail.com>
2021-09-16 15:08:40 +1200

    Create - Xylophone application by following London App Bewer

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,109 +8,135 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      title: 'Xylephone',
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            SizedBox(
+              height: 30.0,
+            ),
+            CutomeButton(
+              audiofileName: 'note1.wav',
+              keyColor: Color(0xFFDE0000),
+            ),
+            CutomeButton(
+              audiofileName: 'note2.wav',
+              keyColor: Color(0xFFFE622C),
+            ),
+            CutomeButton(
+              audiofileName: 'note3.wav',
+              keyColor: Color(0xFFFEF600),
+            ),
+            CutomeButton(
+              audiofileName: 'note4.wav',
+              keyColor: Color(0xFF00BC00),
+            ),
+            CutomeButton(
+              audiofileName: 'note5.wav',
+              keyColor: Color(0xFF009CFE),
+            ),
+            CutomeButton(
+              audiofileName: 'note6.wav',
+              keyColor: Color(0xFF000084),
+            ),
+            CutomeButton(
+              audiofileName: 'note7.wav',
+              keyColor: Color(0xFF2C009C),
+            ),
+            BottomButton(),
+          ],
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class BottomButton extends StatelessWidget {
+  const BottomButton({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Container(
+      height: 100.0,
+      alignment: Alignment.center,
+      color: Colors.black,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          ColoringLettersWidget(
+            letter: "P",
+            letterColor: Color(0xFFDE0000),
+          ),
+          ColoringLettersWidget(
+            letter: "I",
+            letterColor: Color(0xFFFEF600),
+          ),
+          ColoringLettersWidget(
+            letter: "A",
+            letterColor: Color(0xFF00BC00),
+          ),
+          ColoringLettersWidget(
+            letter: "N",
+            letterColor: Color(0xFF009CFE),
+          ),
+          ColoringLettersWidget(
+            letter: "O",
+            letterColor: Colors.red,
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    );
+  }
+}
+
+class ColoringLettersWidget extends StatelessWidget {
+  const ColoringLettersWidget({
+    Key? key,
+    required this.letter,
+    required this.letterColor,
+  }) : super(key: key);
+
+  final String letter;
+  final Color letterColor;
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      letter,
+      style: TextStyle(
+        fontSize: 50.0,
+        color: letterColor,
+      ),
+    );
+  }
+}
+
+class CutomeButton extends StatelessWidget {
+  const CutomeButton({required this.audiofileName, required this.keyColor});
+  final String audiofileName;
+  final Color keyColor;
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: () {
+          final audioPlayer = AudioCache();
+          audioPlayer.play(audiofileName);
+        },
+        child: Container(
+          margin: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 1.0),
+          height: 80.0,
+          width: double.infinity,
+          color: keyColor,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
